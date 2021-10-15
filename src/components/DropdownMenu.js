2673491d02572252c1180e1f329f055f6e75d8a3
willewilsonson <william.nordenstad@gmail.com>
2021-10-15 11:08:18 +0200

    cleaned up dropdownmenu

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -4,14 +4,14 @@ const DropdownMenu = ({ showSection, setShowSection, handleClick }) => {
 
     return (
         <div className='menu-wrapper'>
-        <button onClick={ () => handleClick() } className='menu-wrapper__back-button'>MENU</button>
-        <div className='menu-wrapper__dropdown'>
-            { showSection !== 'portfolio' ? <button onClick={ () => setShowSection('portfolio') }>PORTFOLIO</button> : '' }
-            { showSection !== 'skills' ? <button onClick={ () => setShowSection('skills') }>SKILLS</button> : '' }
-            { showSection !== 'about' ? <button onClick={ () => setShowSection('about') }>ABOUT</button> : '' }
-            { showSection !== 'contact' ? <button onClick={ () => setShowSection('contact') }>CONTACT</button> : '' }
-            <button onClick={ () => handleClick() }>BACK</button>
-        </div>
+            <button onClick={ () => handleClick() } className='menu-wrapper__back-button'>MENU</button>
+            <div className='menu-wrapper__dropdown'>
+                { showSection !== 'portfolio' ? <button onClick={ () => setShowSection('portfolio') }>PORTFOLIO</button> : '' }
+                { showSection !== 'skills' ? <button onClick={ () => setShowSection('skills') }>SKILLS</button> : '' }
+                { showSection !== 'about' ? <button onClick={ () => setShowSection('about') }>ABOUT</button> : '' }
+                { showSection !== 'contact' ? <button onClick={ () => setShowSection('contact') }>CONTACT</button> : '' }
+                <button onClick={ () => handleClick() }>BACK</button>
+            </div>
         </div>
     );
 };
