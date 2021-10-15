6493ab4047376f884d838e3fcbef9da09b217fd6
willewilsonson <william.nordenstad@gmail.com>
2021-10-15 11:06:22 +0200

    css fixes

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -3,13 +3,14 @@ import './DropdownMenu.css';
 const DropdownMenu = ({ showSection, setShowSection, handleClick }) => {
 
     return (
-        <div className='meny-wrapper'>
+        <div className='menu-wrapper'>
         <button onClick={ () => handleClick() } className='menu-wrapper__back-button'>MENU</button>
-        <div className='meny-wrapper__dropdown'>
+        <div className='menu-wrapper__dropdown'>
             { showSection !== 'portfolio' ? <button onClick={ () => setShowSection('portfolio') }>PORTFOLIO</button> : '' }
             { showSection !== 'skills' ? <button onClick={ () => setShowSection('skills') }>SKILLS</button> : '' }
             { showSection !== 'about' ? <button onClick={ () => setShowSection('about') }>ABOUT</button> : '' }
             { showSection !== 'contact' ? <button onClick={ () => setShowSection('contact') }>CONTACT</button> : '' }
+            <button onClick={ () => handleClick() }>BACK</button>
         </div>
         </div>
     );
